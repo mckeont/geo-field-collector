f7830287c4292af76225c14b66b9cc108e53bb17
Tom <mckeont@upenn.edu>
2023-08-22 10:39:41 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,4 +1,4 @@
-var map = L.map('map', {center: [26.846466, 31.378611], zoom: 5, zoomControl: false});
+var map = L.map('map', {center: [29.846466, 31.378611], zoom: 5, zoomControl: false});
 var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
