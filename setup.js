b35caf706253e3ef5d56847cbe1e303f227ce708
Tom <mckeont@upenn.edu>
2023-08-22 10:17:05 -0400

    Update and rename setup2.js to setup.js

--- setup.js
+++ setup.js
@@ -1,11 +1,7 @@
 var map = L.map('map', {center: [26.846466, 31.378611],zoom: 10, zoomControl: false});
-
 var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
-
-
-
 L.control.zoom({
     position: 'bottomright'
 }).addTo(map);
